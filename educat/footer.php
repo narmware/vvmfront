9e5d960c4b1f25fa90c90e82e035d0d4d881cf01
surajchandrakantyadav <suraj@narmware.com>
2018-07-03 14:53:57 +0530

    Delete old files

---
+++ educat/footer.php
@@ -0,0 +1,201 @@
+                <!--Newsletter Area Start-->
+                <div class="newsletter-area">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-5 col-sm-5">
+                                <div class="newsletter-content">
+                                    <h3>SUBSCRIBE</h3>
+                                    <h2>TO OUR NEWSLETTER</h2>
+                                </div>
+                            </div>
+                            <div class="col-md-7 col-sm-7">
+                                <div class="newsletter-form angle">
+                                    <form action="#" id="mc-form" class="mc-form footer-newsletter fix">
+                                        <div class="subscribe-form">
+                                            <input id="mc-email" type="email" name="email" placeholder="Enter your email address...">
+                                            <button id="mc-submit" type="submit">SUBSCRIBE</button>
+                                        </div>    
+                                    </form>
+                                    <!-- mailchimp-alerts Start -->
+                                    <div class="mailchimp-alerts text-centre fix pull-right">
+                                        <div class="mailchimp-submitting"></div><!-- mailchimp-submitting end -->
+                                        <div class="mailchimp-success"></div><!-- mailchimp-success end -->
+                                        <div class="mailchimp-error"></div><!-- mailchimp-error end -->
+                                    </div>
+                                    <!-- mailchimp-alerts end -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--End of Newsletter Area-->
+                <!--Footer Widget Area Start-->
+                <div class="footer-widget-area">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-3 col-sm-4">
+                                <div class="single-footer-widget">
+                                    <div class="footer-logo">
+                                        <a href="index.html"><img src="img/logo/footer.png" alt=""></a>
+                                    </div>
+                                    <p>There are many variations of passg of Lorem Ipsum available, but thmajority have suffered altem, </p>
+                                    <div class="social-icons">
+                                        <a href="#"><i class="zmdi zmdi-facebook"></i></a>
+                                        <a href="#"><i class="zmdi zmdi-rss"></i></a>
+                                        <a href="#"><i class="zmdi zmdi-google-plus"></i></a>
+                                        <a href="#"><i class="zmdi zmdi-pinterest"></i></a>
+                                        <a href="#"><i class="zmdi zmdi-instagram"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-3 col-sm-4">
+                                <div class="single-footer-widget">
+                                    <h3>GET IN TOUCH</h3>
+                                    <span><i class="fa fa-phone"></i>+88 018 785 4589</span>
+                                    <span><i class="fa fa-envelope"></i>devitems@email.com</span>
+                                    <span><i class="fa fa-globe"></i>www.devitems.com</span>
+                                    <span><i class="fa fa-map-marker"></i>ur address goes here,street.</span>
+                                </div>
+                            </div>
+                            <div class="col-md-3 hidden-sm">
+                                <div class="single-footer-widget">
+                                    <h3>Useful Links</h3>
+                                    <ul class="footer-list">
+                                        <li><a href="#">Teachers &amp; Staff</a></li>
+                                        <li><a href="#">Our Courses</a></li>
+                                        <li><a href="#">Courses Categories</a></li>
+                                        <li><a href="#">Support</a></li>
+                                        <li><a href="#">Terms &amp; Conditions</a></li>
+                                        <li><a href="#">Privacy Policy</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-md-3 col-sm-4">
+                                <div class="single-footer-widget">
+                                    <h3>Instagram</h3>
+                                    <div class="instagram-image">
+                                        <div class="footer-img">
+                                            <a href="#"><img src="img/footer/1.jpg" alt=""></a>
+                                        </div>
+                                        <div class="footer-img">
+                                            <a href="#"><img src="img/footer/2.jpg" alt=""></a>
+                                        </div>
+                                        <div class="footer-img">
+                                            <a href="#"><img src="img/footer/3.jpg" alt=""></a>
+                                        </div>
+                                        <div class="footer-img">
+                                            <a href="#"><img src="img/footer/4.jpg" alt=""></a>
+                                        </div>
+                                        <div class="footer-img">
+                                            <a href="#"><img src="img/footer/5.jpg" alt=""></a>
+                                        </div>
+                                        <div class="footer-img">
+                                            <a href="#"><img src="img/footer/6.jpg" alt=""></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--End of Footer Widget Area-->
+                <!--Footer Area Start-->
+                <footer class="footer-area">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-6 col-sm-7">
+                                <span>Copyright &copy; EDUCAT 2016.All right reserved.Created by <a href="#">Devitems</a></span>
+                            </div>
+                            <div class="col-md-6 col-sm-5">
+                                <div class="column-right">
+                                    <span>Privacy Policy , Terms &amp; Conditions</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+                <!--End of Footer Area-->
+            </div>   
+            <!--End of Bg White--> 
+        </div>    
+        <!--End of Main Wrapper Area--> 
+        
+        
+		<!-- jquery
+		============================================ -->		
+        <script src="js/vendor/jquery-1.12.4.min.js"></script>
+        
+		<!-- bootstrap JS
+		============================================ -->		
+        <script src="js/bootstrap.min.js"></script>
+        
+        <!-- nivo slider js
+		============================================ -->       
+		<script src="lib/nivo-slider/js/jquery.nivo.slider.js" type="text/javascript"></script>
+		<script src="lib/nivo-slider/home.js" type="text/javascript"></script>
+        
+		<!-- meanmenu JS
+		============================================ -->		
+        <script src="js/jquery.meanmenu.js"></script>
+		
+		<!-- wow JS
+		============================================ -->		
+        <script src="js/wow.min.js"></script>
+        
+		<!-- owl.carousel JS
+		============================================ -->		
+        <script src="js/owl.carousel.min.js"></script>
+        
+		<!-- scrollUp JS
+		============================================ -->		
+        <script src="js/jquery.scrollUp.min.js"></script>
+        
+		<!-- Waypoints JS
+		============================================ -->		
+        <script src="js/waypoints.min.js"></script>
+        
+		<!-- Counterup JS
+		============================================ -->		
+        <script src="js/jquery.counterup.min.js"></script>
+        
+		<!-- Slick JS
+		============================================ -->		
+        <script src="js/slick.min.js"></script>
+        
+		<!-- Animated Headlines JS
+		============================================ -->		
+        <script src="js/animated-headlines.js"></script>
+        
+		<!-- Textilate JS
+		============================================ -->		
+        <script src="js/textilate.js"></script>
+        
+		<!-- Lettering JS
+		============================================ -->		
+        <script src="js/lettering.js"></script>
+        
+		<!-- Video Player JS
+		============================================ -->		
+        <script src="js/jquery.mb.YTPlayer.js"></script>
+        
+		<!-- Mail Chimp JS
+		============================================ -->		
+        <script src="js/jquery.ajaxchimp.min.js"></script>
+        
+		<!-- AJax Mail JS
+		============================================ -->		
+        <script src="js/ajax-mail.js"></script>
+        
+		<!-- plugins JS
+		============================================ -->		
+        <script src="js/plugins.js"></script>
+        
+		<!-- StyleSwitch JS
+		============================================ -->	
+        <script src="js/styleswitch.js"></script>
+        
+		<!-- main JS
+		============================================ -->		
+        <script src="js/main.js"></script>
+    </body>
+</html>
