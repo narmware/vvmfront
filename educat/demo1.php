f50facc455baa5ff69cd808c3ed38994cbefd9b7
narmwarepoojadevkar <pooja.d@narmware.com>
2018-07-05 18:38:53 +0530

    Made changes & linked completed web pages

---
+++ educat/demo1.php
@@ -0,0 +1,247 @@
+<?php
+        include "header.php";
+?>
+
+
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>About || Educat</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+		<!-- favicon
+		============================================ -->		
+        <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
+		
+		<!-- Google Fonts
+		============================================ -->		
+        <link href='https://fonts.googleapis.com/css?family=Raleway:400,300,500,600,700,800' rel='stylesheet' type='text/css'>
+	   
+		<!-- Bootstrap CSS
+		============================================ -->		
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        
+		<!-- Color Swithcer CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/color-switcher.css">
+        
+		<!-- Fontawsome CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        
+		<!-- Owl Carousel CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/owl.carousel.css">
+        
+		<!-- jquery-ui CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/jquery-ui.css">
+        
+		<!-- Meanmenu CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/meanmenu.min.css">
+        
+		<!-- Animate CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/animate.css">
+        
+		<!-- Animated Headlines CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/animated-headlines.css">
+        
+        <!-- Nivo slider CSS
+		============================================ -->
+		<link rel="stylesheet" href="lib/nivo-slider/css/nivo-slider.css" type="text/css" />
+		<link rel="stylesheet" href="lib/nivo-slider/css/preview.css" type="text/css" media="screen" />
+        
+		<!-- Metarial Iconic Font CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/material-design-iconic-font.css">
+        <link rel="stylesheet" href="css/material-design-iconic-font.min.css">
+        
+		<!-- Slick CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/slick.css">
+        
+		<!-- Video CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/jquery.mb.YTPlayer.css">
+        
+		<!-- Style CSS
+		============================================ -->
+        <link rel="stylesheet" href="style.css">
+        
+		<!-- Color CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/color.css">
+        
+		<!-- Responsive CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/responsive.css">
+        
+		<!-- Modernizr JS
+		============================================ -->		
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+	    
+        <!-- Color Css Files
+		============================================ -->	
+        <link rel="alternate stylesheet" type="text/css" href="switcher/color-one.css" title="color-one" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/color-two.css" title="color-two" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/color-three.css" title="color-three" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/color-four.css" title="color-four" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/color-five.css" title="color-five" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/color-six.css" title="color-six" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/color-seven.css" title="color-seven" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/color-eight.css" title="color-eight" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/color-nine.css" title="color-nine" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/color-ten.css" title="color-ten" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/color-ten.css" title="color-ten" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/pattren1.css" title="pattren1" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/pattren2.css" title="pattren2" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/pattren3.css" title="pattren3" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/pattren4.css" title="pattren4" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/pattren5.css" title="pattren5" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/background1.css" title="background1" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/background2.css" title="background2" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/background3.css" title="background3" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/background4.css" title="background4" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/background5.css" title="background5" media="screen" />
+    </head>
+<style>
+t{
+	text-align :left;
+	font-size : 18px;
+}
+</style>
+
+<!--About Page Area Start-->
+                <div class="about-page-area section-padding">
+                    <div class="container">
+					 <div class="row">
+                            <div class="col-md-12">
+                                <div>
+                                    <div>
+                                        <h4 style="color:#2d3e50">Students participating in VVM shall undergo the following multi-level testing procedures:</h4>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div>
+                                <div >
+                                    <div class="section-title">
+                                        <t><h3>Students participating in VVM shall undergo the following multi-level testing procedures:</h3></t>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div >
+                                <div class="about-text-container">
+                                    <p><span>Well come to Educat</span> come with us, we also teach children about the basic values of human beings as honesty, kindness, generosity, courage, freedom, equality and respect. Learn to celebrate diversity in a spirit of understanding and tolerance and develop a positive regard and awareness of other people. They are taught the values and responsibilities needed to become active members of the community, tolerance and develop something which the modern world is desperate for norem ipsum dolor sit amet desperate.<br>
+                                    Adipiscing signiferumque vix et. No alii docendi usu, pri graeco possim percipit ne.</p>
+                                    <div class="about-us">
+                                        <span>Nam gravida magna vitae ante dignissim</span>
+                                        <span>Duis rhoncus lectus at velit hendrerit quis</span>
+                                        <span>Sed fringilla tempor arcu feugiat risus</span>
+                                        <span>Vivamus semper odio in nibh ultricies</span>
+                                    </div>    
+                                    <p>Snos trud exerci tation ullorper suscipit lobo maeisrn roester maeoirqs iserrtis nisl ut aliq poerwse thesr oper balinp asein hoinws mawsoib equat. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more tolerance and develop obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through makin of the word in classical literature.</p>
+                                </div>
+                            </div>
+                           
+                        </div>
+                    </div>
+                </div>
+                <!--End of About Page Area-->
+				
+				
+				  
+		<!-- jquery
+		============================================ -->		
+        <script src="js/vendor/jquery-1.12.4.min.js"></script>
+        
+		<!-- bootstrap JS
+		============================================ -->		
+        <script src="js/bootstrap.min.js"></script>
+        
+        <!-- nivo slider js
+		============================================ -->       
+		<script src="lib/nivo-slider/js/jquery.nivo.slider.js" type="text/javascript"></script>
+		<script src="lib/nivo-slider/home.js" type="text/javascript"></script>
+        
+		<!-- meanmenu JS
+		============================================ -->		
+        <script src="js/jquery.meanmenu.js"></script>
+		
+		<!-- wow JS
+		============================================ -->		
+        <script src="js/wow.min.js"></script>
+        
+		<!-- owl.carousel JS
+		============================================ -->		
+        <script src="js/owl.carousel.min.js"></script>
+        
+		<!-- scrollUp JS
+		============================================ -->		
+        <script src="js/jquery.scrollUp.min.js"></script>
+        
+		<!-- Waypoints JS
+		============================================ -->		
+        <script src="js/waypoints.min.js"></script>
+        
+		<!-- Counterup JS
+		============================================ -->		
+        <script src="js/jquery.counterup.min.js"></script>
+        
+		<!-- Slick JS
+		============================================ -->		
+        <script src="js/slick.min.js"></script>
+        
+		<!-- Animated Headlines JS
+		============================================ -->		
+        <script src="js/animated-headlines.js"></script>
+        
+		<!-- Textilate JS
+		============================================ -->		
+        <script src="js/textilate.js"></script>
+        
+		<!-- Lettering JS
+		============================================ -->		
+        <script src="js/lettering.js"></script>
+        
+		<!-- Video Player JS
+		============================================ -->		
+        <script src="js/jquery.mb.YTPlayer.js"></script>
+        
+		<!-- Mail Chimp JS
+		============================================ -->		
+        <script src="js/jquery.ajaxchimp.min.js"></script>
+        
+		<!-- AJax Mail JS
+		============================================ -->		
+        <script src="js/ajax-mail.js"></script>
+        
+		<!-- plugins JS
+		============================================ -->		
+        <script src="js/plugins.js"></script>
+        
+		<!-- StyleSwitch JS
+		============================================ -->	
+        <script src="js/styleswitch.js"></script>
+        
+		<!-- main JS
+		============================================ -->		
+        <script src="js/main.js"></script>
+    </body>
+</html>
+
+
+<?php
+        include "footer.php";
+?>
---
+++ educat/structure_of_vm.php
@@ -0,0 +1,215 @@
+<?php
+        include "header.php";
+?>
+
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Home One || Educat</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+		<!-- favicon
+		============================================ -->		
+        <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
+		
+		<!-- Google Fonts
+		============================================ -->		
+        <link href='https://fonts.googleapis.com/css?family=Raleway:400,300,500,600,700,800' rel='stylesheet' type='text/css'>
+	   
+		<!-- Bootstrap CSS
+		============================================ -->		
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        
+		<!-- Color Swithcer CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/color-switcher.css">
+        
+		<!-- Fontawsome CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        
+		<!-- Owl Carousel CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/owl.carousel.css">
+        
+		<!-- jquery-ui CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/jquery-ui.css">
+        
+		<!-- Meanmenu CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/meanmenu.min.css">
+        
+		<!-- Animate CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/animate.css">
+        
+		<!-- Animated Headlines CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/animated-headlines.css">
+        
+        <!-- Nivo slider CSS
+		============================================ -->
+		<link rel="stylesheet" href="lib/nivo-slider/css/nivo-slider.css" type="text/css" />
+		<link rel="stylesheet" href="lib/nivo-slider/css/preview.css" type="text/css" media="screen" />
+        
+		<!-- Metarial Iconic Font CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/material-design-iconic-font.css">
+        <link rel="stylesheet" href="css/material-design-iconic-font.min.css">
+        
+		<!-- Slick CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/slick.css">
+        
+		<!-- Video CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/jquery.mb.YTPlayer.css">
+        
+		<!-- Style CSS
+		============================================ -->
+        <link rel="stylesheet" href="style.css">
+        
+		<!-- Color CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/color.css">
+        
+		<!-- Responsive CSS
+		============================================ -->
+        <link rel="stylesheet" href="css/responsive.css">
+        
+		<!-- Modernizr JS
+		============================================ -->		
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+	    
+        <!-- Color Css Files
+		============================================ -->	
+        <link rel="alternate stylesheet" type="text/css" href="switcher/color-one.css" title="color-one" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/color-two.css" title="color-two" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/color-three.css" title="color-three" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/color-four.css" title="color-four" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/color-five.css" title="color-five" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/color-six.css" title="color-six" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/color-seven.css" title="color-seven" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/color-eight.css" title="color-eight" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/color-nine.css" title="color-nine" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/color-ten.css" title="color-ten" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/color-ten.css" title="color-ten" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/pattren1.css" title="pattren1" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/pattren2.css" title="pattren2" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/pattren3.css" title="pattren3" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/pattren4.css" title="pattren4" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/pattren5.css" title="pattren5" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/background1.css" title="background1" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/background2.css" title="background2" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/background3.css" title="background3" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/background4.css" title="background4" media="screen" />
+        <link rel="alternate stylesheet" type="text/css" href="switcher/background5.css" title="background5" media="screen" />
+    </head>
+		<style>
+
+
+p1 {
+      
+		
+		 font-color:#2d3e50;
+}
+
+
+b { 
+   font-weight: bold;
+   	 font-color:#2d3e50;
+}
+	
+
+
+b1{
+	 font-size:14px;
+	 	 font-color:#2d3e50;
+}
+
+t{
+		 font-color:#2d3e50;
+}
+}
+</style>
+    <body>
+	
+	
+	
+	
+	<!--Breadcrumb Banner Area Start-->
+                <div class="breadcrumb-banner-area">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="breadcrumb-text">
+                                    <h1 class="text-center">STRUCTURE OF VIDYARTHI VIGYAN MANTHAN
+</h1>
+                                    <div class="breadcrumb-bar">
+                                        <ul class="breadcrumb text-center">
+                                            <li><a href="index.html">Home</a></li>
+                                            <li> STRUCTURE OF VVM</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--End of Breadcrumb Banner Area-->
+				
+				
+				<!--Text Area Start-->
+				 <div class="mainbox">
+                <div class="text-area pt-30 pb-50">
+                    <div class="container">
+                        <div class="row">
+                           <div class="row">
+                            <div class="col-md-12">
+                                <div>
+                                    <div>
+                                        <h4 style="color:#2d3e50">Students participating in VVM shall undergo the following multi-level testing procedures:</h4>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                            <div class="col-md-12">
+                                <div class="mt-20">
+                                    
+						
+                <ul>
+                    <li style="color:#2d3e50">&nbsp;&nbsp;&nbsp;&nbsp;<span class="glyphicon glyphicon-arrow-right" style="color:#2d3e50"></span>&nbsp;Objective type question answering. </li><br>
+                    <li style="color:#2d3e50">&nbsp;&nbsp;&nbsp;&nbsp;<span class="glyphicon glyphicon-arrow-right" style="color:#2d3e50"></span>&nbsp;Comprehensive writing.</li><br>
+                    <li style="color:#2d3e50">&nbsp;&nbsp;&nbsp;&nbsp;<span class="glyphicon glyphicon-arrow-right" style="color:#2d3e50"></span>&nbsp;Presentation and Group discussion.</li><br>
+                    <li style="color:#2d3e50">&nbsp;&nbsp;&nbsp;&nbsp;<span class="glyphicon glyphicon-arrow-right" style="color:#2d3e50"></span>&nbsp;Role play & Practical examination.</li><br>
+                    <li style="color:#2d3e50">&nbsp;&nbsp;&nbsp;&nbsp;<span class="glyphicon glyphicon-arrow-right" style="color:#2d3e50"></span>&nbsp;Methods of Source.</li><br>
+               </ul>
+                <p1 style="color:#2d3e50">
+                    The program also includes organising visits to reputed National Science Laboratories and Centres. To enable deserving students to have interaction with renowned scientists of the Nation. The VVM will identify Vigyan Jigyasu (विज्ञानं जिज्ञासु, Keen Knowledge Seeker of Science) at National level.
+                </p1><br><br>
+                <p1 style="color:#2d3e50">
+                    <b>Appearing student will be evaluated and compared with respect to the students from his/her own class at all the stages i.e. School, State and National level.</b>
+                </p1>
+                
+                                </div>
+                            </div>  
+                        </div>
+                    </div>
+                </div>
+				</div>
+                <!--End of Text Area-->
+				
+				
+
+
+				
+				   </body>
+</html>
+<?php
+        include "footer.php";
+?>
